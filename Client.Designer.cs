95124a165dcf5e92ef6d3784db359f60f19a1f64
hungbb <thanhhung94@gmail.com>
2014-10-22 21:00:20 +0700

    TCPChat
    Chat đơn giản theo giao thức TCP

---
+++ Client.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace TCPChat
+{
+    partial class Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtIpServer = new System.Windows.Forms.TextBox();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtChat = new System.Windows.Forms.TextBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtIpServer
+            // 
+            this.txtIpServer.Location = new System.Drawing.Point(69, 28);
+            this.txtIpServer.Name = "txtIpServer";
+            this.txtIpServer.Size = new System.Drawing.Size(122, 20);
+            this.txtIpServer.TabIndex = 0;
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(69, 66);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(122, 20);
+            this.txtUsername.TabIndex = 1;
+            // 
+            // txtChat
+            // 
+            this.txtChat.Location = new System.Drawing.Point(15, 107);
+            this.txtChat.Multiline = true;
+            this.txtChat.Name = "txtChat";
+            this.txtChat.Size = new System.Drawing.Size(257, 241);
+            this.txtChat.TabIndex = 2;
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(197, 63);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 3;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Server IP";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Username";
+            // 
+            // txtSend
+            // 
+            this.txtSend.Enabled = false;
+            this.txtSend.Location = new System.Drawing.Point(15, 367);
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(193, 20);
+            this.txtSend.TabIndex = 6;
+            this.txtSend.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSend_KeyPress);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(214, 364);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(58, 23);
+            this.btnSend.TabIndex = 7;
+            this.btnSend.Text = "Gửi";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 415);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.txtChat);
+            this.Controls.Add(this.txtUsername);
+            this.Controls.Add(this.txtIpServer);
+            this.Name = "Client";
+            this.Text = "TCPClient";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Client_FormClosing);
+            this.Load += new System.EventHandler(this.Client_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtIpServer;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtChat;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ Client.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Threading;
+
+namespace TCPChat
+{
+    public partial class Client : Form
+    {
+        private string Username = "Thanh Hung";
+        private StreamWriter swSender;
+        private StreamReader srReceiver;
+        private TcpClient tcpServer;
+
+        private delegate void UpdateLogCallback(string strMessage);
+
+        private delegate void CloseConnectionCallback(string strReason);
+        private Thread thrMessaging;
+        private IPAddress ipAddr;
+        private bool Connected;
+
+        public Client()
+        {
+            Application.ApplicationExit += new EventHandler(OnApplicationExit);
+            InitializeComponent();
+            
+        }
+
+        private void OnApplicationExit(object sender, EventArgs e)
+        {
+            if (Connected == true)
+            {
+                Connected = false;
+                swSender.Close();
+                srReceiver.Close();
+                tcpServer.Close();
+            }
+        }
+
+        
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (Connected == false)
+            {
+                initConnection(); //thiết lập kết nối
+            }
+            else
+            {
+                CloseConnection("Ngắt kết nối bởi user");
+            }
+        }
+
+        private void initConnection()
+        {
+            ipAddr = IPAddress.Parse(txtIpServer.Text);
+
+            tcpServer = new TcpClient();
+            tcpServer.Connect(ipAddr,2014);
+
+            Connected = true;
+            Username = txtUsername.Text;
+
+            txtIpServer.Enabled = false;
+            txtUsername.Enabled = false;
+            txtSend.Enabled = true;
+            btnConnect.Text = "Disconnect";
+
+            swSender = new StreamWriter(tcpServer.GetStream());
+            swSender.WriteLine(txtUsername.Text);
+            swSender.Flush();
+
+            thrMessaging = new Thread(new ThreadStart(ReceiveMessage));
+            thrMessaging.Start();
+        }
+
+        private void ReceiveMessage()
+        {
+            srReceiver = new StreamReader(tcpServer.GetStream());
+            string ConResponse = srReceiver.ReadLine();
+            if (ConResponse[0] == '1')
+            {
+                this.Invoke(new UpdateLogCallback(this.UpdateLog), new object[] { "Kết nối thành công!" });
+            }
+            else
+            {
+                string Reason = "Không kết nối!";
+                Reason += ConResponse.Substring(2, ConResponse.Length - 2);
+                this.Invoke(new CloseConnectionCallback(this.CloseConnection), new object[] { Reason });
+                return;
+            }
+            while (Connected)
+            {
+                //Hiển  thị tin nhắn trong textbox chat
+                if (ConResponse[0] == '1')
+                this.Invoke(new UpdateLogCallback(this.UpdateLog), new object[] { srReceiver.ReadLine() });
+
+            }
+        }
+
+        private void UpdateLog(string strMessage)
+        {
+            txtChat.AppendText(strMessage + "\r\n");
+        }
+        private void CloseConnection(string Reason)
+        {
+            txtChat.AppendText(Reason + "\r\n");
+            txtIpServer.Enabled = true;
+            txtUsername.Enabled = true;
+            txtSend.Enabled = false;
+           btnConnect.Text = "Connect";
+
+            Connected = false;
+            swSender.Close();
+            srReceiver.Close();
+           
+            tcpServer.Close();
+        }
+
+        private void SendMessage()
+        {
+            if (txtSend.Lines.Length >= 1)
+            {
+                swSender.WriteLine(txtSend.Text);
+                swSender.Flush();
+                txtUsername.Lines = null;
+            }
+            txtSend.Text = "";
+        }
+    
+
+        private void Client_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Client_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            CloseConnection("");
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            SendMessage();
+        }
+
+        private void txtSend_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)13)
+            {
+                SendMessage();
+            }
+        }
+    }
+}
